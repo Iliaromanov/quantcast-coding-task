cc4547b763dea9f0c00cf547998623e3d6acee86
Iliaromanov <iliaromanov033@gmail.com>
2024-02-04 17:26:13 -0500

    python CLI program and bash script wrapper

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,5 +1,6 @@
 import sys
-from CookieLogParser import CookieLogParser
+import os.path
+from cookie_log_parser import CookieLogParser
 
 
 def main():
@@ -7,7 +8,19 @@ def main():
         print("Usage: most_active_cookie <cookie_log_file_path.csv> -d <utc_timezone>")
         sys.exit(1)
 
-    print(f"$1 = {sys.argv[1]}, $3 = {sys.argv[2]}")
+    path = sys.argv[1]
+    date = sys.argv[2]
+    if not os.path.exists(path):
+        print(f"Path '{path}' is not a valid path. " + 
+              "Try entering full path if relative path doesn't work.")
+        exit(1)
+
+    cookie_log_parser = CookieLogParser()
+    cookie_log_parser.parse_csv_to_dict(path)
+    most_active = cookie_log_parser.get_most_active_cookie(date)
+
+    for cookie in most_active:
+        print(cookie)
 
 
 if __name__ == "__main__":
